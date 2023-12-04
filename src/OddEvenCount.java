90a324228f02673e13b860db0afa804762610415
sangamb11 <sangambhosale11@gmail.com>
2023-12-04 14:09:37 +0530

    initial commit

---
+++ src/OddEvenCount.java
@@ -0,0 +1,14 @@
+
+public class OddEvenCount {
+
+	public static void main(String[] args) {
+		String s1 = new String("Sunbeam");
+		String s2 = "Sunbeam";
+		if (s1 == s2)
+			System.out.println("Equal");
+		else
+			System.out.println("Not Equal");
+
+	}
+
+}
